ae69417dea9e25cdb62e48ea8d1b652627d8202a
rstubryan <restubumirr@gmail.com>
2025-05-10 22:23:22 +0700

    feat: implement container and main content layout components

---
+++ src/components/templates/content/main-content.tsx
@@ -0,0 +1,56 @@
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Separator } from "@/components/ui/separator";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { ModeToggle } from "@/components/molecules/themes/toggle-theme";
+import ContainerLayout from "@/components/templates/content/container-layout";
+
+export default function MainContent({
+  children,
+  className,
+}: {
+  children?: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <ContainerLayout>
+      <section>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center justify-between w-full px-4">
+            <div className="flex items-center gap-2">
+              <SidebarTrigger className="-ml-1" />
+              <Separator
+                orientation="vertical"
+                className="mr-2 data-[orientation=vertical]:h-4"
+              />
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem className="hidden md:block">
+                    <BreadcrumbLink href="#">
+                      Building Your Application
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator className="hidden md:block" />
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
+            <ModeToggle />
+          </div>
+        </header>
+      </section>
+      <Separator />
+      <section className={`container mx-auto my-4 sm:px-0 px-4 ${className}`}>
+        {children}
+      </section>
+    </ContainerLayout>
+  );
+}
