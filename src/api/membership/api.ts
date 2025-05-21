db8ded864308f281ad967c35730a2475b49471c4
rstubryan <restubumirr@gmail.com>
2025-05-21 14:18:45 +0700

    feat: implement membership API and hooks for CRUD operations

---
+++ src/api/membership/api.ts
@@ -0,0 +1,47 @@
+import { createApiRequest } from "../base/api-factory";
+import type { ResponseProps } from "../base/global-type";
+import type {
+  MembershipProps,
+  CreateMembershipProps,
+  EditMembershipProps,
+} from "./type";
+
+const BASE_URL = `/memberships`;
+
+export const membershipApi = {
+  getAllMemberships: createApiRequest<void, ResponseProps<MembershipProps[]>>({
+    endpoint: `${BASE_URL}`,
+    method: "GET",
+  }),
+
+  createMembership: createApiRequest<
+    CreateMembershipProps,
+    ResponseProps<MembershipProps>
+  >({
+    endpoint: `${BASE_URL}`,
+    method: "POST",
+  }),
+
+  updateMembership: createApiRequest<
+    Partial<EditMembershipProps>,
+    ResponseProps<MembershipProps>
+  >({
+    endpoint: `${BASE_URL}`,
+    method: "PATCH",
+    extraConfig: ({ urlParams }) => ({
+      params: {
+        id: urlParams?.id,
+      },
+    }),
+  }),
+
+  deleteMembership: createApiRequest<void, ResponseProps<null>>({
+    endpoint: `${BASE_URL}`,
+    method: "DELETE",
+    extraConfig: ({ urlParams }) => ({
+      params: {
+        id: urlParams?.id,
+      },
+    }),
+  }),
+};
